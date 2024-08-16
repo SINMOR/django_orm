d8130fcedb119e5f236cdd33f291542ca902ed62
Sindet <morrisindet@gmail.com>
2024-08-16 17:42:38 +0300

    first commit

---
+++ orm_series/core/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here. models can also be called tables 
+
+# Restaurant
+# User
+# Rating 
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    website = models.URLField(default="")
+    date_opened = models.DateField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    
+    def __str__(self):
+        return self.name
